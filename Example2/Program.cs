8080b44d27af120c13b4db8bce79473f0da5640c
Leonid Tolstov <leonski@mail.ru>
2023-02-11 14:05:36 +0300

    Max from 3 digits

---
+++ Example2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Максимальное из трех введенных чисел
+Console.WriteLine("Enter number 1 ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter number 2 ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter number 3 ");
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+if(b>a)
+{
+    max=b;
+}
+if(c>b)
+{
+    max=c;
+}
+Console.WriteLine("Maximum = " + max);
