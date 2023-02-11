21e57bf1992dd729afd356789fb9e6cc5af0278e
Leonid Tolstov <leonski@mail.ru>
2023-02-11 16:43:21 +0300

    Четные числа от 1 до N

---
+++ Example4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести четные числа от 1 до N
+Console.WriteLine("Введите целое число: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Четные числа до " + N + ":");
+for (int i=1; i<N; i++)
+{
+    if(i%2==0)
+    {
+        Console.Write(i + ", ");
+    }
+
+}  
