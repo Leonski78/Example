3f03b4b897387a07fe928ce6aff18718ff166a98
Leonid Tolstov <leonski@mail.ru>
2023-02-11 16:49:07 +0300

    Проверка на четность

---
+++ Example3-1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверка числа на четность
+Console.WriteLine("Введите целое число: ");
+int a = int.Parse(Console.ReadLine());
+if (a%2==0)
+    {
+        Console.WriteLine("Число " + a + " четное");
+    }
+else
+    {
+        Console.WriteLine("Число " + a + " не четное");
+    } 
