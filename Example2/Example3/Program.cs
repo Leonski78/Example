9bc89ce01e7e626fb5997169326b70229dd0dd06
Leonid Tolstov <leonski@mail.ru>
2023-02-11 15:19:53 +0300

    %2 commit

---
+++ Example2/Example3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число: ");
+int a = int.Parse(Console.ReadLine());
+if (a%2==0)
+    {
+        Console.WriteLine("Число " + a + " четное");
+    }
+if (a%2==1)
+    {
+        Console.WriteLine("Число " + a + " не четное");
+    }                                                 
