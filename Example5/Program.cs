04a2606e446ac48d5c887bda1401eecc651e2519
Leonid Tolstov <leonski@mail.ru>
2023-02-14 18:07:05 +0300

    second digit

---
+++ Example5/Program.cs
@@ -0,0 +1,7 @@
+﻿// программа, которая принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int N = int.Parse(Console.ReadLine());
+int Second = (N % 100 - N % 10)/10;
+Console.WriteLine($"Вторая цифра числа{N} -> {Second}");
