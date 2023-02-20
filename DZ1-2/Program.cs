fc8941c7371031124f5646a7ec813edf2792e2bf
Leonid Tolstov <leonski@mail.ru>
2023-02-20 10:04:48 +0300

    polindrom dz1

--- DZ1-2/Program.cs
+++ DZ1-2/Program.cs
@@ -1,12 +1,22 @@
 ﻿// программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-Console.Clear();
-Console.WriteLine("Введите пятизначное число N: ");
-int N = int.Parse(Console.ReadLine()!);
+// Варианты:
 // var n = 12345;
 // var arr = n.toString().split('');
 // for (i = 0; i < arr.length; i++) arr[i] = +arr[i] | 0;
-int[] N = new int[5];
-if (N[0]=N[4] + N[1]=N[3])
+// int[] N = new int[5];
+// if (N[0]=N[4] + N[1]=N[3])
+Console.Clear();
+Console.WriteLine("Введите пятизначное число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int Temp = N;
+int rev = 0;
+while (N>0)
+{
+    int dig = N % 10;
+    rev = rev * 10 + dig;
+    N = N/10;
+}
+if (Temp == rev)
 {
     Console.WriteLine("Это палиндром !");
 }
