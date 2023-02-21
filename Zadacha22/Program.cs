bf278b70239cf1f428177222e7eb4ef11a468e49
Leonid Tolstov <leonski@mail.ru>
2023-02-21 14:57:46 +0300

    вывод массива до  max

--- Zadacha22/Program.cs
+++ Zadacha22/Program.cs
@@ -1,15 +1,14 @@
 ﻿//30 Напишите программу, которая выводит массив из N элементов, 
-//заполненный нулями и единицами в случайном порядке.
-// и массивом из любых натуральных чисел Next()
+//заполненный любыми натуральными числами до Max -> Next(Max)
 
-//[1,0,1,1,0,1,0,0]
+//[1,0,1,1,0,1,0,0] - бинарный вариант
 // a = new Random().Next()
 // int a = new Random().Next(2)
 
 Console.Clear();
 Console.WriteLine("Введите число: ");
 int N = int.Parse(Console.ReadLine());
-int[] array = BinaryArray(N);
+int[] array = GetArray(N);
 // первый вариант вывода массива:
 Console.WriteLine($"[{String.Join(", " , array)}]");
 // второй вариант вывода массива:
@@ -19,7 +18,7 @@ for (int i = 0; i < N-1; i++)
 }
 Console.Write($"{array[N-1]}");
 
-int[] BinaryArray (int size)
+int[] GetArray (int size)
 {
     int[] result = new int [size];
     for (int i =0; i < size; i ++)
