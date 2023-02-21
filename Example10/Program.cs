ab92360ba5ba006558c5ec3d9dd579ae3918255c
Leonid Tolstov <leonski@mail.ru>
2023-02-21 14:51:41 +0300

    вывод массива

--- Example10/Program.cs
+++ Example10/Program.cs
@@ -4,15 +4,40 @@
 //4-> 24
 //5-> 120 == 1 * 2 * 3 * 4 * 5
 
-int GetNumber(int n) 
+int Factorial(int n) 
 {
   int result = 1;
   for (int i = 1; i <= n; i++){
-      result = result * i;
+      result *= i;//result = result * i;
   }
   return result;
 }
 
 Console.Write("Введите число: ");
 int n = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"{n} -> {GetNumber(n)}");
+Console.WriteLine($"Фаториал !{n} -> {Factorial(n)}");
+
+
+// для отрицательных
+int GetMultiply(int n){ // n=3
+    int result = 1;
+    int i = 2;
+    if( n < 0){
+        i = n;
+        n = -1;
+    }
+    for (; i <= n; i++){ // i <= n; 2 <= 3
+        result *= i; // result
+    }
+    return result;
+}
+
+//30 Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+
+//[1,0,1,1,0,1,0,0]
+// a = new Random().Next()
+// int a = new Random().Next(2)
+int[] arrray = BinaryArray(10);
+Console.WriteLine($"{String.Join(", " , array)}");
+
---
+++ Zadacha22/Program.cs
@@ -0,0 +1,32 @@
+﻿//30 Напишите программу, которая выводит массив из N элементов, 
+//заполненный нулями и единицами в случайном порядке.
+// и массивом из любых натуральных чисел Next()
+
+//[1,0,1,1,0,1,0,0]
+// a = new Random().Next()
+// int a = new Random().Next(2)
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int[] array = BinaryArray(N);
+// первый вариант вывода массива:
+Console.WriteLine($"[{String.Join(", " , array)}]");
+// второй вариант вывода массива:
+for (int i = 0; i < N-1; i++)
+{
+    Console.Write($"{array[i]}, ");
+}
+Console.Write($"{array[N-1]}");
+
+int[] BinaryArray (int size)
+{
+    int[] result = new int [size];
+    for (int i =0; i < size; i ++)
+    {
+        result[i] = new Random ().Next(100);
+    }
+        return result;
+}
+
+
