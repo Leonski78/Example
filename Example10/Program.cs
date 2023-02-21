0636504f79270641f4aeea4645f43f3db3a88120
Leonid Tolstov <leonski@mail.ru>
2023-02-21 10:56:38 +0300

    factorial

---
+++ Example10/Program.cs
@@ -0,0 +1,18 @@
+﻿//28 Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N.
+
+//4-> 24
+//5-> 120 == 1 * 2 * 3 * 4 * 5
+
+int GetNumber(int n) 
+{
+  int result = 1;
+  for (int i = 1; i <= n; i++){
+      result = result * i;
+  }
+  return result;
+}
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{n} -> {GetNumber(n)}");
---
+++ Example9/Program.cs
@@ -0,0 +1,21 @@
+﻿// 26 Напишите программу,
+//  которая принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int GetNumber(int n) 
+{
+  int count = 0;
+  while (n > 0) {
+    n = n/10;
+    count++;
+  }
+
+  return count; 
+}
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{n} -> {GetNumber(n)}");
